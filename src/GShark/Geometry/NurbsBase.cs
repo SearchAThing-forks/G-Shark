f9666e0fc8ca62be732b6663f01473cb89911e52
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-04-13 02:33:31 +0200

    sample with fix applied

--- src/GShark/Geometry/NurbsBase.cs
+++ src/GShark/Geometry/NurbsBase.cs
@@ -659,8 +659,8 @@ namespace GShark.Geometry
                 var sNext = Vector3.CrossProduct(pointsOnCurveTan[i + 1], rNext); //compute vector s[i+1] of next frame
 
                 //create output frame
-                // var frameNext = new Plane(pointsOnCurve[i + 1], rNext, sNext);  
-                var frameNext = new Plane { Origin = pointsOnCurve[i + 1], XAxis = rNext, YAxis = sNext };              
+                var frameNext = new Plane(pointsOnCurve[i + 1], rNext, sNext);  
+                // var frameNext = new Plane { Origin = pointsOnCurve[i + 1], XAxis = rNext, YAxis = sNext };              
                 perpFrames[i + 1] = frameNext; //output frame
             }
 
